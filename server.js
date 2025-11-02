ef5b240290002d755bb6dfa7e8fa012a5b4b3945
Pranay roy <pranayroy374@gmail.com>
2025-11-02 22:10:40 +0530

    Initial setup with modules and mongodb connection

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+
+
+dotenv.config();// loads all .env variables into process.env
+const app=express();// create a server
+const PORT=process.env.PORT || 4000;
+
+//application middlewares
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use(morgan("dev"));
+
+//routes
+app.get('/',()=> console.log("default loading route"))
+
+//start the server;
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`server running on port ${PORT}`)
+});
